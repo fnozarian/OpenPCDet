c5eaf71a9099f364f267dc0c28edc54c6fca20e7
Farzad <fnozarian@gmail.com>
2021-04-05 11:05:44 +0200

    Added NuscenesDatasetKITTIEval to dataset __init__.

--- pcdet/datasets/__init__.py
+++ pcdet/datasets/__init__.py
@@ -7,11 +7,13 @@ from pcdet.utils import common_utils
 from .dataset import DatasetTemplate
 from .kitti.kitti_dataset import KittiDataset
 from .nuscenes.nuscenes_dataset import NuScenesDataset
+from .nuscenes.nuscenes_dataset_kitti_eval import NuScenesDatasetKITTIEval
 
 __all__ = {
     'DatasetTemplate': DatasetTemplate,
     'KittiDataset': KittiDataset,
-    'NuScenesDataset': NuScenesDataset
+    'NuScenesDataset': NuScenesDataset,
+    'NuScenesDatasetKITTIEval': NuScenesDatasetKITTIEval
 }
 
 
