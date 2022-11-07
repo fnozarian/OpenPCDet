c783a40f02cffe1d462b2aa5b137b487c9ba8b37
shashankag14 <shashankag14@gmail.com>
2022-11-07 19:26:49 +0100

    DA-9 Convert unbounded angles to bounded

--- pcdet/models/detectors/pv_rcnn_ssl.py
+++ pcdet/models/detectors/pv_rcnn_ssl.py
@@ -733,6 +733,9 @@ class PVRCNN_SSL(Detector3DTemplate):
             batch_dict[key][unlabeled_inds], batch_dict_org['rot_angle'][unlabeled_inds])
         batch_dict[key][unlabeled_inds] = global_scaling_bbox(
             batch_dict[key][unlabeled_inds], batch_dict_org['scale'][unlabeled_inds])
+        
+        batch_dict[key][unlabeled_inds, :, 6] = common_utils.limit_period(
+            batch_dict[key][unlabeled_inds, :, 6], offset=0.5, period=2 * np.pi)
         return batch_dict
     
     def reverse_augmentation(self, batch_dict, batch_dict_org, unlabeled_inds, key = 'rois'):
@@ -744,6 +747,9 @@ class PVRCNN_SSL(Detector3DTemplate):
             batch_dict[key][unlabeled_inds], batch_dict_org['flip_y'][unlabeled_inds])
         batch_dict[key][unlabeled_inds] = random_flip_along_x_bbox(
             batch_dict[key][unlabeled_inds], batch_dict_org['flip_x'][unlabeled_inds])
+        
+        batch_dict[key][unlabeled_inds, :, 6] = common_utils.limit_period(
+            batch_dict[key][unlabeled_inds, :, 6], offset=0.5, period=2 * np.pi)
         return batch_dict
 
     def get_supervised_training_loss(self):
