3f9acfbe0cfda8b594c5565d5f29f88b6b134aeb
Farzad <fnozarian@gmail.com>
2022-12-14 19:37:57 +0100

    Bugfix: reverse_augmentation.

--- pcdet/models/detectors/pv_rcnn_ssl.py
+++ pcdet/models/detectors/pv_rcnn_ssl.py
@@ -604,16 +604,21 @@ class PVRCNN_SSL(Detector3DTemplate):
         )
 
         return batch_dict
-    
-    def reverse_augmentation(self, batch_dict, batch_dict_org, unlabeled_inds, key = 'rois'):
+
+    def reverse_augmentation(self, batch_dict, batch_dict_org, unlabeled_inds, key='rois'):
         batch_dict[key][unlabeled_inds] = global_scaling_bbox(
-            batch_dict[key][unlabeled_inds], batch_dict_org['scale'][unlabeled_inds])
+            batch_dict[key][unlabeled_inds], 1.0 / batch_dict_org['scale'][unlabeled_inds])
         batch_dict[key][unlabeled_inds] = global_rotation_bbox(
-            batch_dict[key][unlabeled_inds], batch_dict_org['rot_angle'][unlabeled_inds])
+            batch_dict[key][unlabeled_inds], - batch_dict_org['rot_angle'][unlabeled_inds])
         batch_dict[key][unlabeled_inds] = random_flip_along_y_bbox(
             batch_dict[key][unlabeled_inds], batch_dict_org['flip_y'][unlabeled_inds])
         batch_dict[key][unlabeled_inds] = random_flip_along_x_bbox(
             batch_dict[key][unlabeled_inds], batch_dict_org['flip_x'][unlabeled_inds])
+
+        batch_dict[key][unlabeled_inds, :, 6] = common_utils.limit_period(
+            batch_dict[key][unlabeled_inds, :, 6], offset=0.5, period=2 * np.pi
+        )
+
         return batch_dict
 
     def get_supervised_training_loss(self):
