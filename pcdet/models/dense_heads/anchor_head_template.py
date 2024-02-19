* Introduce weighted thresholding/PL (only for rcnn reg loss for now). * Introduce joint dist align. * Bugfix stats_utils precision. * Cleanup

pcdet/models/dense_heads/anchor_head_template.py
@@ -98,7 +98,7 @@ class AnchorHeadTemplate(nn.Module):
         )
         return targets_dict
 
-    def get_cls_layer_loss(self, scalar=True):
+    def get_cls_layer_loss(self):
         cls_preds = self.forward_ret_dict['cls_preds']
         box_cls_labels = self.forward_ret_dict['box_cls_labels']
         batch_size = int(cls_preds.shape[0])
@@ -127,18 +127,15 @@ class AnchorHeadTemplate(nn.Module):
         cls_preds = cls_preds.view(batch_size, -1, self.num_class)
         one_hot_targets = one_hot_targets[..., 1:]
         cls_loss_src = self.cls_loss_func(cls_preds, one_hot_targets, weights=cls_weights)  # [N, M]
-        if scalar:
-            cls_loss = cls_loss_src.sum() / batch_size
-            rpn_acc_cls = ((cls_preds.max(-1)[1] + 1) == cls_targets.long()).sum().float() / \
-                          torch.clamp((cls_targets > 0).sum().float(), min=1.0)
-        else:
-            cls_loss = cls_loss_src.reshape(batch_size, -1).sum(-1)
-            rpn_acc_cls = ((cls_preds.max(-1)[1] + 1) == cls_targets.long()).view(batch_size, -1).sum(-1).float() / \
-                          torch.clamp((cls_targets > 0).view(batch_size, -1).sum(-1).float(), min=1.0)
+
+        cls_loss = cls_loss_src.reshape(batch_size, -1).sum(-1)
+        correct_preds = (cls_preds.max(-1)[1] + 1) == cls_targets.long()
+        rpn_acc_cls = (correct_preds & positives).sum(-1).float() / torch.clamp(positives.sum(-1).float(), min=1.0)
 
         cls_loss = cls_loss * self.model_cfg.LOSS_CONFIG.LOSS_WEIGHTS['cls_weight']
         tb_dict = {
-            'rpn_loss_cls': cls_loss.item() if scalar else cls_loss,
+            'rpn_loss_cls': cls_loss,
+            'rpn_acc_cls': rpn_acc_cls
         }
 
         return cls_loss, tb_dict
@@ -168,7 +165,7 @@ class AnchorHeadTemplate(nn.Module):
             dir_cls_targets = dir_targets
         return dir_cls_targets
 
-    def get_box_reg_layer_loss(self, scalar=True):
+    def get_box_reg_layer_loss(self):
         box_preds = self.forward_ret_dict['box_preds']
         box_dir_cls_preds = self.forward_ret_dict.get('dir_cls_preds', None)
         box_reg_targets = self.forward_ret_dict['box_reg_targets']
@@ -196,15 +193,13 @@ class AnchorHeadTemplate(nn.Module):
         # sin(a - b) = sinacosb-cosasinb
         box_preds_sin, reg_targets_sin = self.add_sin_difference(box_preds, box_reg_targets)
         loc_loss_src = self.reg_loss_func(box_preds_sin, reg_targets_sin, weights=reg_weights)  # [N, M]
-        if scalar:
-            loc_loss = loc_loss_src.sum() / batch_size
-        else:
-            loc_loss = loc_loss_src.reshape(batch_size, -1).sum(-1)
+
+        loc_loss = loc_loss_src.reshape(batch_size, -1).sum(-1)
 
         loc_loss = loc_loss * self.model_cfg.LOSS_CONFIG.LOSS_WEIGHTS['loc_weight']
         box_loss = loc_loss
         tb_dict = {
-            'rpn_loss_loc': loc_loss.item() if scalar else loc_loss
+            'rpn_loss_loc': loc_loss
         }
 
         if box_dir_cls_preds is not None:
@@ -218,29 +213,23 @@ class AnchorHeadTemplate(nn.Module):
             weights = positives.type_as(dir_logits)
             weights /= torch.clamp(weights.sum(-1, keepdim=True), min=1.0)
             dir_loss = self.dir_loss_func(dir_logits, dir_targets, weights=weights)
-            if scalar:
-                dir_loss = dir_loss.sum() / batch_size
-            else:
-                dir_loss = dir_loss.reshape(batch_size, -1).sum(-1)
+
+            dir_loss = dir_loss.reshape(batch_size, -1).sum(-1)
 
             dir_loss = dir_loss * self.model_cfg.LOSS_CONFIG.LOSS_WEIGHTS['dir_weight']
             box_loss += dir_loss
-            tb_dict['rpn_loss_dir'] = dir_loss.item() if scalar else dir_loss
+            tb_dict['rpn_loss_dir'] = dir_loss
 
         return box_loss, tb_dict
 
-    def get_loss(self, scalar=True):
-        cls_loss, tb_dict = self.get_cls_layer_loss(scalar=scalar)
-        box_loss, tb_dict_box = self.get_box_reg_layer_loss(scalar=scalar)
+    def get_loss(self):
+        cls_loss, tb_dict = self.get_cls_layer_loss()
+        box_loss, tb_dict_box = self.get_box_reg_layer_loss()
         tb_dict.update(tb_dict_box)
         rpn_loss = cls_loss + box_loss
 
-        if scalar:
-            tb_dict['rpn_loss'] = rpn_loss.item()
-            return rpn_loss, tb_dict
-        else:
-            tb_dict['rpn_loss'] = rpn_loss
-            return cls_loss, box_loss, tb_dict
+        tb_dict['rpn_loss'] = rpn_loss
+        return cls_loss, box_loss, tb_dict
 
     def generate_predicted_boxes(self, batch_size, cls_preds, box_preds, dir_cls_preds=None):
         """

pcdet/models/detectors/pv_rcnn_ssl.py
@@ -54,7 +54,8 @@ class PVRCNN_SSL(Detector3DTemplate):
             feature_bank_registry.register(tag=bank_configs["NAME"], **bank_configs)
 
         for metrics_configs in model_cfg.get("METRICS_BANK_LIST", []):
-            metrics_registry.register(tag=metrics_configs["NAME"], dataset=self.dataset, **metrics_configs)
+            if metrics_configs.ENABLE:
+                metrics_registry.register(tag=metrics_configs["NAME"], dataset=self.dataset, **metrics_configs)
 
         vals_to_store = ['iou_roi_pl', 'iou_roi_gt', 'pred_scores', 'teacher_pred_scores',
                          'weights', 'roi_scores', 'pcv_scores', 'num_points_in_roi', 'class_labels', 'iteration']
@@ -87,7 +88,7 @@ class PVRCNN_SSL(Detector3DTemplate):
             sample_mask = batch_dict['points'][:, 0].int() == ix
             points = batch_dict['points'][sample_mask, 1:4]
             gt_feat = batch_gt_feats[ix][nonzero_mask]
-            gt_labels = gt_boxes[:, -1].int() - 1
+            gt_labels = gt_boxes[:, 7].int() - 1
             gt_boxes = gt_boxes[:, :7]
             ins_idxs = batch_dict['instance_idx'][ix][nonzero_mask].int()
             smpl_id = torch.from_numpy(batch_dict['frame_id'].astype(np.int32))[ix].to(gt_boxes.device)
@@ -187,21 +188,26 @@ class PVRCNN_SSL(Detector3DTemplate):
         batch_dict_ema = self._split_batch(batch_dict, tag='ema')
         self._gen_pseudo_labels(batch_dict_ema)
         pls_teacher_wa, _ = self.pv_rcnn_ema.post_processing(batch_dict_ema, no_recall_dict=True)
-        pseudo_boxes, pseudo_scores, pseudo_sem_scores, pseudo_sem_logits = self._filter_pls_conf_scores(pls_teacher_wa, ulb_inds)
-        ulb_pred_labels = torch.cat([pb[:, -1] for pb in pseudo_boxes]).int().detach()
+        pl_boxes, pl_scores, pl_sem_scores, pl_sem_logits = self._filter_pls_conf_scores(pls_teacher_wa, ulb_inds)
+        ulb_pred_labels = torch.cat([pb[:, -1] for pb in pl_boxes]).int().detach()
         pl_cls_count_pre_filter = torch.bincount(ulb_pred_labels, minlength=4)[1:]
 
         # Semantic Filtering
-        filtering_masks, ps_sem_scores_rect = self._filter_pls_sem_scores(pseudo_sem_logits)
+        filtering_masks, pl_sem_scores_rect = self._filter_pls_sem_scores(pl_scores, pl_sem_logits)
 
-        self.update_metrics(pseudo_boxes, ps_sem_scores_rect, filtering_masks, batch_dict_ema['gt_boxes'][ulb_inds])
+        # TODO(farzad): Set the scores of pseudo-labels that their argmax is changed after rectification to zero.
+        pl_weights = [scores_rect.max(-1, keepdim=True)[0] for scores_rect in pl_sem_scores_rect]
+        # pl_weights = [scores.new_ones(scores.shape[0], 1) for scores in pl_sem_scores_rect]
+        if 'pl_metrics' in metrics_registry.tags():
+            metrics_input = self.get_pl_metrics_input(pl_boxes, pl_sem_scores_rect, pl_weights, filtering_masks, batch_dict_ema['gt_boxes'][ulb_inds])
+            metrics_registry.get('pl_metrics').update(**metrics_input)
 
-        pseudo_boxes = [boxes[mask] for boxes, mask in zip(pseudo_boxes, filtering_masks)]
+        pl_boxes = [torch.hstack([boxes[mask], weights[mask]]) for boxes, weights, mask in zip(pl_boxes, pl_weights, filtering_masks)]
 
         # Comment the following line to use gt boxes for unlabeled data!
-        self._fill_with_pseudo_labels(batch_dict, pseudo_boxes, ulb_inds, lbl_inds)
+        self._fill_with_pseudo_labels(batch_dict, pl_boxes, ulb_inds, lbl_inds)
 
-        pl_cls_count_post_filter = torch.bincount(batch_dict['gt_boxes'][ulb_inds][...,-1].view(-1).int().detach(), minlength=4)[1:]
+        pl_cls_count_post_filter = torch.bincount(batch_dict['gt_boxes'][ulb_inds][...,7].view(-1).int().detach(), minlength=4)[1:]
         gt_cls_count = torch.bincount(batch_dict['ori_unlabeled_boxes'][...,-1].view(-1).int().detach(), minlength=4)[1:]
 
         pl_count_dict = {'avg_num_gts_per_sample': self._arr2dict(gt_cls_count / len(ulb_inds)),  # backward compatibility. Added to stats_utils. Will be removed later.
@@ -233,7 +239,7 @@ class PVRCNN_SSL(Detector3DTemplate):
             pl_scores_lbl = [pls_teacher_wa[i]['pred_scores'] for i in lbl_inds]
             pl_sem_logits_lbl = [pls_teacher_wa[i]['pred_sem_scores_logits'] for i in lbl_inds]
             conf_scores_wa_lbl, sem_scores_wa_lbl = self._get_thresh_alg_inputs(pl_scores_lbl, pl_sem_logits_lbl)
-            conf_scores_wa_ulb, sem_scores_wa_ulb = self._get_thresh_alg_inputs(pseudo_scores, pseudo_sem_logits)
+            conf_scores_wa_ulb, sem_scores_wa_ulb = self._get_thresh_alg_inputs(pl_scores, pl_sem_logits)
             thresh_inputs['conf_scores_wa'] = torch.cat([conf_scores_wa_lbl, conf_scores_wa_ulb])
             thresh_inputs['sem_scores_wa'] = torch.cat([sem_scores_wa_lbl, sem_scores_wa_ulb])
             thresh_inputs['gt_labels_wa'] = self.pad_tensor(batch_dict_ema['gt_boxes'][..., 7:8], max_len=100).detach().clone()
@@ -255,9 +261,9 @@ class PVRCNN_SSL(Detector3DTemplate):
                 self._add_teacher_scores(batch_dict, batch_dict_ema, ulb_inds)
 
         disp_dict = {}
-        loss_rpn_cls, loss_rpn_box, tb_dict = self.pv_rcnn.dense_head.get_loss(scalar=False)
-        loss_point, tb_dict = self.pv_rcnn.point_head.get_loss(tb_dict, scalar=False)
-        loss_rcnn_cls, loss_rcnn_box, tb_dict = self.pv_rcnn.roi_head.get_loss(tb_dict, scalar=False)
+        loss_rpn_cls, loss_rpn_box, tb_dict = self.pv_rcnn.dense_head.get_loss()
+        loss_point, tb_dict = self.pv_rcnn.point_head.get_loss(tb_dict)
+        loss_rcnn_cls, loss_rcnn_box, tb_dict = self.pv_rcnn.roi_head.get_loss(tb_dict)
 
         loss = 0
         # Use the same reduction method as the baseline model (3diou) by the default
@@ -317,7 +323,7 @@ class PVRCNN_SSL(Detector3DTemplate):
         gt_boxes = batch_dict['gt_boxes']
         B, N = gt_boxes.shape[:2]
         sa_pl_feats = self.pv_rcnn.roi_head.pool_features(batch_dict, use_gtboxes=True).view(B * N, -1)
-        pl_labels = batch_dict['gt_boxes'][..., -1].view(-1).long() - 1
+        pl_labels = batch_dict['gt_boxes'][..., 7].view(-1).long() - 1
         proto_cont_loss = bank.get_proto_contrastive_loss(sa_pl_feats, pl_labels)
         if proto_cont_loss is None:
             return
@@ -452,7 +458,8 @@ class PVRCNN_SSL(Detector3DTemplate):
         file_path = os.path.join(output_dir, 'scores.pkl')
         pickle.dump(self.val_dict, open(file_path, 'wb'))
 
-    def update_metrics(self, rois, sem_scores, filtering_masks, gts, tag='pl_metrics'):
+    @staticmethod
+    def get_pl_metrics_input(rois, sem_scores, weights, filtering_masks, gts):
         metrics_input = defaultdict(list)
         bs = len(rois)
         for i in range(bs):
@@ -461,15 +468,17 @@ class PVRCNN_SSL(Detector3DTemplate):
                 # TODO: fix stats for no preds case
                 smpl_rois = gts[i].new_zeros((1, 8)).float().to(gts[i].device)  # dummy rois
                 smpl_scores = gts[i].new_zeros((1, 3)).float().to(gts[i].device)
+                smpl_weights = gts[i].new_zeros((1, 1)).float().to(gts[i].device)
             else:
                 smpl_rois = rois[i][smpl_mask].clone().detach()
                 smpl_scores = sem_scores[i][smpl_mask].clone().detach()
+                smpl_weights = weights[i][smpl_mask].clone().detach()
 
             metrics_input['rois'].append(smpl_rois)
             metrics_input['roi_scores'].append(smpl_scores)
             metrics_input['ground_truths'].append(gts[i].clone().detach())
-
-        metrics_registry.get(tag).update(**metrics_input)
+            metrics_input['roi_weights'].append(smpl_weights)
+        return metrics_input
 
     @staticmethod
     def _unpack_preds(pred_dicts, ulb_inds):
@@ -519,17 +528,17 @@ class PVRCNN_SSL(Detector3DTemplate):
                 sem_scores_logits_list.append(sem_scores_logits[mask])
                 pseudo_scores.append(scores[mask])
         return pseudo_boxes, pseudo_scores, pseudo_sem_scores, sem_scores_logits_list
-    def _filter_pls_sem_scores(self, batch_roi_sem_scores_logtis):
+    def _filter_pls_sem_scores(self, batch_conf_scores, batch_sem_logtis):
         masks = []
         scores_list = []
-        for logits in batch_roi_sem_scores_logtis:
+        for conf_scores, sem_logits in zip(batch_conf_scores, batch_sem_logtis):
             if self.thresh_alg:
-                mask, scores = self.thresh_alg.get_mask(logits)
+                mask, scores = self.thresh_alg.get_mask(conf_scores, sem_logits)
             else:  # 3dioumatch baseline
-                labels = torch.argmax(logits, dim=1)
-                scores = torch.sigmoid(logits)
+                labels = torch.argmax(sem_logits, dim=1)
+                scores = torch.sigmoid(sem_logits)
                 max_scores = scores.max(dim=-1)[0]
-                sem_conf_thresh = torch.tensor(self.sem_thresh, device=logits.device).unsqueeze(0).expand_as(logits).gather(dim=1, index=labels.unsqueeze(-1)).squeeze()
+                sem_conf_thresh = torch.tensor(self.sem_thresh, device=sem_logits.device).unsqueeze(0).expand_as(sem_logits).gather(dim=1, index=labels.unsqueeze(-1)).squeeze()
                 mask = max_scores > sem_conf_thresh
 
             masks.append(mask)
@@ -541,6 +550,13 @@ class PVRCNN_SSL(Detector3DTemplate):
         key = 'gt_boxes' if key is None else key
         max_box_num = batch_dict['gt_boxes'].shape[1]
 
+        # Expand the gt_boxes to have the same shape as the pseudo_boxes
+        gt_scores = torch.zeros((batch_dict['gt_boxes'].shape[0], max_box_num, 1), device=batch_dict['gt_boxes'].device)
+        batch_dict['gt_boxes'] = torch.cat([batch_dict['gt_boxes'], gt_scores], dim=-1)
+        # Make sure that scores of labeled boxes are always 1, except for the padding rows which should remain zero.
+        valid_inds_lbl = torch.logical_not(torch.eq(batch_dict['gt_boxes'][labeled_inds], 0).all(dim=-1)).nonzero().long()
+        batch_dict['gt_boxes'][valid_inds_lbl[:, 0], valid_inds_lbl[:, 1], 8] = 1
+
         # Ignore the count of pseudo boxes if filled with default values(zeros) when no preds are made
         max_pseudo_box_num = max(
             [torch.logical_not(torch.all(ps_box == 0, dim=-1)).sum().item() for ps_box in pseudo_boxes])
@@ -549,24 +565,23 @@ class PVRCNN_SSL(Detector3DTemplate):
             for i, pseudo_box in enumerate(pseudo_boxes):
                 diff = max_box_num - pseudo_box.shape[0]
                 if diff > 0:
-                    pseudo_box = torch.cat([pseudo_box, torch.zeros((diff, 8), device=pseudo_box.device)], dim=0)
+                    pseudo_box = torch.cat([pseudo_box, torch.zeros((diff, pseudo_box.shape[-1]), device=pseudo_box.device)], dim=0)
                 batch_dict[key][unlabeled_inds[i]] = pseudo_box
         else:
             ori_boxes = batch_dict['gt_boxes']
             ori_ins_ids = batch_dict['instance_idx']
-            new_boxes = torch.zeros((ori_boxes.shape[0], max_pseudo_box_num, ori_boxes.shape[2]),
-                                    device=ori_boxes.device)
+            new_boxes = torch.zeros((ori_boxes.shape[0], max_pseudo_box_num, ori_boxes.shape[-1]), device=ori_boxes.device)
             new_ins_idx = torch.full((ori_boxes.shape[0], max_pseudo_box_num), fill_value=-1, device=ori_boxes.device)
             for idx in labeled_inds:
                 diff = max_pseudo_box_num - ori_boxes[idx].shape[0]
-                new_box = torch.cat([ori_boxes[idx], torch.zeros((diff, 8), device=ori_boxes[idx].device)], dim=0)
+                new_box = torch.cat([ori_boxes[idx], torch.zeros((diff, ori_boxes.shape[-1]), device=ori_boxes[idx].device)], dim=0)
                 new_boxes[idx] = new_box
                 new_ins_idx[idx] = torch.cat([ori_ins_ids[idx], -torch.ones((diff,), device=ori_boxes[idx].device)], dim=0)
             for i, pseudo_box in enumerate(pseudo_boxes):
 
                 diff = max_pseudo_box_num - pseudo_box.shape[0]
                 if diff > 0:
-                    pseudo_box = torch.cat([pseudo_box, torch.zeros((diff, 8), device=pseudo_box.device)], dim=0)
+                    pseudo_box = torch.cat([pseudo_box, torch.zeros((diff, pseudo_box.shape[-1]), device=pseudo_box.device)], dim=0)
                 new_boxes[unlabeled_inds[i]] = pseudo_box
             batch_dict[key] = new_boxes
             batch_dict['instance_idx'] = new_ins_idx

pcdet/models/roi_heads/roi_head_template.py
@@ -13,7 +13,7 @@ from ...ops.iou3d_nms import iou3d_nms_utils
 from collections import defaultdict
 from pcdet.config import cfg # temporarily adding cfg for temprature scaling
 
-def update_metrics(targets_dict, mask_type='cls'):
+def get_roi_metrics_input(targets_dict, mask_type='cls'):
     metrics_input = defaultdict(list)
 
     for i, uind in enumerate(targets_dict['unlabeled_inds']):
@@ -50,8 +50,7 @@ def update_metrics(targets_dict, mask_type='cls'):
     if len(metrics_input['rois']) == 0:
         # print(f'Warning: No {mask_type} rois for any unlabeled index')
         return
-    tag = f'rcnn_roi_pl_gt_metrics_{mask_type}'
-    metrics_registry.get(tag).update(**metrics_input)
+    return metrics_input
 
 
 class RoIHeadTemplate(nn.Module):
@@ -238,13 +237,14 @@ class RoIHeadTemplate(nn.Module):
         else:
             raise ValueError(f"Invalid array shape: {array.shape}")
 
-    def get_box_reg_layer_loss(self, forward_ret_dict, scalar=True):
+    def get_box_reg_layer_loss(self, forward_ret_dict):
         loss_cfgs = self.model_cfg.LOSS_CONFIG
         code_size = self.box_coder.code_size
 
         reg_valid_mask = forward_ret_dict['reg_valid_mask'].view(-1)
         gt_boxes3d_ct = forward_ret_dict['gt_of_rois'][..., 0:code_size]
         gt_of_rois_src = forward_ret_dict['gt_of_rois_src'][..., 0:code_size].view(-1, code_size)
+        gt_of_rois_loss_weight = forward_ret_dict['gt_of_rois'][..., -1].view(-1)
         rcnn_reg = forward_ret_dict['rcnn_reg']  # (rcnn_batch_size, C)
         roi_boxes3d = forward_ret_dict['rois']
         rcnn_batch_size = gt_boxes3d_ct.view(-1, code_size).shape[0]
@@ -267,15 +267,15 @@ class RoIHeadTemplate(nn.Module):
                 rcnn_reg.view(rcnn_batch_size, -1).unsqueeze(dim=0),
                 reg_targets.unsqueeze(dim=0),
             )  # [B, M, 7]
-            if scalar:
-                rcnn_loss_reg = (rcnn_loss_reg.view(rcnn_batch_size, -1) * fg_mask.unsqueeze(dim=-1).float()).sum() \
-                                / max(fg_sum, 1)
-            else:
-                fg_sum_ = fg_mask.reshape(batch_size, -1).long().sum(-1)
-                rcnn_loss_reg = (rcnn_loss_reg.view(rcnn_batch_size, -1) *
-                                 fg_mask.unsqueeze(dim=-1).float()).reshape(batch_size, -1).sum(-1) / torch.clamp(fg_sum_.float(), min=1.0)
+
+            fg_sum_ = fg_mask.reshape(batch_size, -1).long().sum(-1)
+            assert gt_of_rois_loss_weight[:rcnn_batch_size // 2].sum() == rcnn_batch_size // 2
+            rcnn_loss_reg = (rcnn_loss_reg.view(rcnn_batch_size, -1) *
+                             gt_of_rois_loss_weight.view(rcnn_batch_size, -1) *
+                             fg_mask.unsqueeze(dim=-1).float()
+                             ).reshape(batch_size, -1).sum(-1) / torch.clamp(fg_sum_.float(), min=1.0)
             rcnn_loss_reg = rcnn_loss_reg * loss_cfgs.LOSS_WEIGHTS['rcnn_reg_weight']
-            tb_dict['rcnn_loss_reg'] = rcnn_loss_reg.item() if scalar else rcnn_loss_reg
+            tb_dict['rcnn_loss_reg'] = rcnn_loss_reg
 
             if loss_cfgs.CORNER_LOSS_REGULARIZATION and fg_sum > 0:
                 split_size = []
@@ -304,23 +304,21 @@ class RoIHeadTemplate(nn.Module):
                     rcnn_boxes3d[:, 0:7],
                     gt_of_rois_src[fg_mask][:, 0:7]
                 )
-                if scalar:
-                    loss_corner = loss_corner.mean()
-                else:
-                    loss_corner = torch.split(loss_corner, split_size, dim=0)
-                    zero = torch.zeros([1], device=fg_mask.device)
-                    loss_corner = [x.mean(dim=0, keepdim=True) if len(x) > 0 else zero for x in loss_corner]
-                    loss_corner = torch.cat(loss_corner, dim=0)
+
+                loss_corner = torch.split(loss_corner, split_size, dim=0)
+                zero = torch.zeros([1], device=fg_mask.device)
+                loss_corner = [x.mean(dim=0, keepdim=True) if len(x) > 0 else zero for x in loss_corner]
+                loss_corner = torch.cat(loss_corner, dim=0)
                 loss_corner = loss_corner * loss_cfgs.LOSS_WEIGHTS['rcnn_corner_weight']
 
                 rcnn_loss_reg += loss_corner
-                tb_dict['rcnn_loss_corner'] = loss_corner.item() if scalar else loss_corner
+                tb_dict['rcnn_loss_corner'] = loss_corner
         else:
             raise NotImplementedError
 
         return rcnn_loss_reg, tb_dict
 
-    def get_box_cls_layer_loss(self, forward_ret_dict, scalar=True):
+    def get_box_cls_layer_loss(self, forward_ret_dict):
         loss_cfgs = self.model_cfg.LOSS_CONFIG
         rcnn_cls = forward_ret_dict['rcnn_cls']
         rcnn_cls_labels = forward_ret_dict['rcnn_cls_labels'].view(-1)
@@ -328,28 +326,22 @@ class RoIHeadTemplate(nn.Module):
             rcnn_cls_flat = rcnn_cls.view(-1)
             batch_loss_cls = F.binary_cross_entropy(torch.sigmoid(rcnn_cls_flat), rcnn_cls_labels.float(), reduction='none')
             cls_valid_mask = (rcnn_cls_labels >= 0).float()
-            if scalar:
-                rcnn_loss_cls = (batch_loss_cls * cls_valid_mask).sum() / torch.clamp(cls_valid_mask.sum(), min=1.0)
+            batch_size = forward_ret_dict['rcnn_cls_labels'].shape[0]
+            batch_loss_cls = batch_loss_cls.reshape(batch_size, -1)
+            cls_valid_mask = cls_valid_mask.reshape(batch_size, -1)
+            if 'rcnn_cls_weights' in forward_ret_dict:
+                rcnn_cls_weights = forward_ret_dict['rcnn_cls_weights']
+                rcnn_loss_cls_norm = (cls_valid_mask * rcnn_cls_weights).sum(-1)
+                rcnn_loss_cls = (batch_loss_cls * cls_valid_mask * rcnn_cls_weights).sum(-1) / torch.clamp(rcnn_loss_cls_norm, min=1.0)
             else:
-                batch_size = forward_ret_dict['rcnn_cls_labels'].shape[0]
-                batch_loss_cls = batch_loss_cls.reshape(batch_size, -1)
-                cls_valid_mask = cls_valid_mask.reshape(batch_size, -1)
-                if 'rcnn_cls_weights' in forward_ret_dict:
-                    rcnn_cls_weights = forward_ret_dict['rcnn_cls_weights']
-                    rcnn_loss_cls_norm = (cls_valid_mask * rcnn_cls_weights).sum(-1)
-                    rcnn_loss_cls = (batch_loss_cls * cls_valid_mask * rcnn_cls_weights).sum(-1) / torch.clamp(rcnn_loss_cls_norm, min=1.0)
-                else:
-                    rcnn_loss_cls = (batch_loss_cls * cls_valid_mask).sum(-1) / torch.clamp(cls_valid_mask.sum(-1), min=1.0)
+                rcnn_loss_cls = (batch_loss_cls * cls_valid_mask).sum(-1) / torch.clamp(cls_valid_mask.sum(-1), min=1.0)
         elif loss_cfgs.CLS_LOSS == 'CrossEntropy':
             batch_loss_cls = F.cross_entropy(rcnn_cls, rcnn_cls_labels, reduction='none', ignore_index=-1)
             cls_valid_mask = (rcnn_cls_labels >= 0).float()
-            if scalar:
-                rcnn_loss_cls = (batch_loss_cls * cls_valid_mask).sum() / torch.clamp(cls_valid_mask.sum(), min=1.0)
-            else:
-                batch_size = forward_ret_dict['rcnn_cls_labels'].shape[0]
-                batch_loss_cls = batch_loss_cls.reshape(batch_size, -1)
-                cls_valid_mask = cls_valid_mask.reshape(batch_size, -1)
-                rcnn_loss_cls = (batch_loss_cls * cls_valid_mask).sum(-1) / torch.clamp(cls_valid_mask.sum(-1), min=1.0)
+            batch_size = forward_ret_dict['rcnn_cls_labels'].shape[0]
+            batch_loss_cls = batch_loss_cls.reshape(batch_size, -1)
+            cls_valid_mask = cls_valid_mask.reshape(batch_size, -1)
+            rcnn_loss_cls = (batch_loss_cls * cls_valid_mask).sum(-1) / torch.clamp(cls_valid_mask.sum(-1), min=1.0)
         elif loss_cfgs.CLS_LOSS == 'UnbiasedCrossEntropy':
 
             tau = self.model_cfg.LOSS_CONFIG.LOSS_WEIGHTS.get('unbiased_ce_tau', 1.0)
@@ -376,7 +368,7 @@ class RoIHeadTemplate(nn.Module):
 
         rcnn_loss_cls = rcnn_loss_cls * loss_cfgs.LOSS_WEIGHTS['rcnn_cls_weight']
         tb_dict = {
-            'rcnn_loss_cls': rcnn_loss_cls.item() if scalar else rcnn_loss_cls
+            'rcnn_loss_cls': rcnn_loss_cls
         }
         return rcnn_loss_cls, tb_dict
 
@@ -468,7 +460,7 @@ class RoIHeadTemplate(nn.Module):
 
         return unlabeled_rcnn_cls_weights
 
-    def get_loss(self, tb_dict=None, scalar=True):
+    def get_loss(self, tb_dict=None):
         tb_dict = {} if tb_dict is None else tb_dict
 
         if not self.model_cfg.ENABLE_SOFT_TEACHER or self.model_cfg.TARGET_CONFIG.DISABLE_ST_WEIGHTS:
@@ -478,22 +470,19 @@ class RoIHeadTemplate(nn.Module):
             unlabeled_inds = self.forward_ret_dict['unlabeled_inds']
             self.forward_ret_dict['rcnn_cls_weights'][unlabeled_inds] = self._get_reliability_weight(unlabeled_inds)
 
-        if self.model_cfg.get("ENABLE_EVAL", None):
+        if 'rcnn_roi_metrics' in metrics_registry.tags():
             # update_metrics(self.forward_ret_dict, mask_type='reg')
-            metrics_pred_types = self.model_cfg.get("METRICS_PRED_TYPES", None)
-            if metrics_pred_types is not None:
-                update_metrics(self.forward_ret_dict, mask_type='cls')
+            metrics_input = get_roi_metrics_input(self.forward_ret_dict, mask_type='cls')
+            metrics_registry.get('rcnn_roi_metrics').update(**metrics_input)
 
-        rcnn_loss_cls, cls_tb_dict = self.get_box_cls_layer_loss(self.forward_ret_dict, scalar=scalar)
-        rcnn_loss_reg, reg_tb_dict = self.get_box_reg_layer_loss(self.forward_ret_dict, scalar=scalar)
+        rcnn_loss_cls, cls_tb_dict = self.get_box_cls_layer_loss(self.forward_ret_dict)
+        rcnn_loss_reg, reg_tb_dict = self.get_box_reg_layer_loss(self.forward_ret_dict)
         rcnn_loss = rcnn_loss_cls + rcnn_loss_reg
         tb_dict.update(cls_tb_dict)
         tb_dict.update(reg_tb_dict)
-        tb_dict['rcnn_loss'] = rcnn_loss.item() if scalar else rcnn_loss
-        if scalar:
-            return rcnn_loss, tb_dict
-        else:
-            return rcnn_loss_cls, rcnn_loss_reg, tb_dict
+        tb_dict['rcnn_loss'] = rcnn_loss
+
+        return rcnn_loss_cls, rcnn_loss_reg, tb_dict
 
     def generate_predicted_boxes(self, batch_size, rois, cls_preds, box_preds):
         """

pcdet/utils/stats_utils.py
@@ -5,7 +5,7 @@ from torch.distributions import Categorical
 import numpy as np
 from pcdet.ops.iou3d_nms import iou3d_nms_utils
 from pcdet.utils import box_utils
-from sklearn.metrics import average_precision_score, confusion_matrix
+from sklearn.metrics import average_precision_score, confusion_matrix, precision_score
 from matplotlib import pyplot as plt
 import itertools
 # from tools.visual_utils import open3d_vis_utils as V
@@ -124,14 +124,12 @@ def plot_confusion_matrix(cm, class_names):
 
   return fig
 
-def _arr2dict(array, ignore_zeros=False):
-    if array.shape[-1] == 2:
-        return {cls: array[cind] for cind, cls in enumerate(['Bg', 'Fg'])}
-    elif array.shape[-1] == 3:
-        return {cls: array[cind] for cind, cls in enumerate(['Car', 'Pedestrian', 'Cyclist']) if array[cind] > 0 or not ignore_zeros}
-    else:
-        raise ValueError(f"Invalid array shape: {array.shape}")
+def _arr2dict(array, ignore_zeros=False, ignore_nan=False):
+    def should_include(value):
+        return not ((ignore_zeros and value == 0) or (ignore_nan and np.isnan(value)))
 
+    classes = ['Bg', 'Fg'] if array.shape[-1] == 2 else ['Car', 'Pedestrian', 'Cyclist']
+    return {cls: array[cind] for cind, cls in enumerate(classes) if should_include(array[cind])}
 class PredQualityMetrics(Metric):
     full_state_update: bool = False
 
@@ -141,7 +139,7 @@ class PredQualityMetrics(Metric):
         self.tag = kwargs.get('tag', None)
         self.dataset = kwargs.get('dataset', None)
 
-        self.states_name = ["roi_scores", "roi_labels", "roi_iou_wrt_gt", "roi_assigned_labels"]
+        self.states_name = ["roi_scores", "roi_weights", "roi_labels", "roi_iou_wrt_gt", "roi_assigned_labels"]
         self.fg_threshs = kwargs.get('fg_threshs', None)
         self.bg_thresh = kwargs.get('BG_THRESH', 0.25)
         self.min_overlaps = np.array([0.7, 0.5, 0.5])
@@ -152,7 +150,7 @@ class PredQualityMetrics(Metric):
         for name in self.states_name:
             self.add_state(name, default=[], dist_reduce_fx='cat')
 
-    def update(self, rois: [torch.Tensor], roi_scores: [torch.Tensor], ground_truths: [torch.Tensor]) -> None:
+    def update(self, rois: [torch.Tensor], roi_scores: [torch.Tensor], roi_weights: [torch.Tensor], ground_truths: [torch.Tensor]) -> None:
 
         _assert_inputs_are_valid(rois, roi_scores, ground_truths)
 
@@ -182,7 +180,7 @@ class PredQualityMetrics(Metric):
             self.roi_iou_wrt_gt.append(sample_roi_iou_wrt_gt.view(-1, 1))
             self.roi_assigned_labels.append(assigned_label.view(-1, 1))
             self.roi_labels.append(sample_roi_labels.view(-1, 1))
-            # self.roi_weights.append(roi_weights[i])
+            self.roi_weights.append(roi_weights[i])
 
         # Draw the last sample in batch
         # pred_boxes = sample_rois[:, :-1].clone().cpu().numpy()
@@ -227,45 +225,47 @@ class PredQualityMetrics(Metric):
         iou_wrt_gt = accumulated_metrics["roi_iou_wrt_gt"].view(-1)
         pred_labels = accumulated_metrics["roi_labels"].view(-1)
         assigned_labels = accumulated_metrics["roi_assigned_labels"].view(-1)
-        # weights = accumulated_metrics["roi_weights"].view(-1)
+        max_scores, argmax_scores = scores.max(dim=-1)
+        weights = accumulated_metrics["roi_weights"].view(-1)
 
         # Multiclass classification average precision score based on different scores.
         y_labels = torch.where(assigned_labels == -1, 3, assigned_labels)
-        one_hot_labels = pred_labels.new_zeros(len(y_labels), 4, dtype=torch.long, device=scores.device)
-        one_hot_labels.scatter_(-1, y_labels.unsqueeze(dim=-1).long(), 1.0).cpu().numpy()
+        # one_hot_labels = argmax_scores.new_zeros(len(y_labels), 4, dtype=torch.long, device=scores.device)
+        # one_hot_labels.scatter_(-1, y_labels.unsqueeze(dim=-1).long(), 1.0).cpu().numpy()
 
         y_labels = y_labels.cpu().numpy()
-        y_scores = np.zeros((len(y_labels), 4))
-        y_scores[:, :3] = scores.cpu().numpy()
+        pred_labels = pred_labels.cpu().numpy()
+        weights = weights.cpu().numpy()
 
         classwise_metrics['mean_p_max_model'] = scores.max(dim=-1)[0].mean().item()
+        mean_p_max_model_classwise = scores.new_zeros((3,)).scatter_(0, scores.max(dim=-1)[1], scores.max(dim=-1)[0]).cpu().numpy()
+        classwise_metrics['mean_p_max_model_classwise'] = _arr2dict(mean_p_max_model_classwise)
         classwise_metrics['mean_p_model'] = _arr2dict(scores.mean(dim=0).cpu().numpy())
+        classwise_metrics['label_hist'] = _arr2dict(torch.bincount(argmax_scores, minlength=3).cpu().numpy() / len(argmax_scores), ignore_zeros=True)
+        precision = precision_score(y_labels, pred_labels, sample_weight=weights, average=None, zero_division=np.nan)
+        classwise_metrics['avg_precision_sem_score'] = _arr2dict(precision[:3], ignore_nan=True)
 
         # sim_scores = accumulated_metrics["roi_sim_scores"]
         # sim_labels = torch.argmax(sim_scores, dim=-1)
         # y_sim_scores = np.zeros((len(y_labels), 4))
         # y_sim_scores[:, :3] = sim_scores.cpu().numpy()
 
-        # y_pred = pred_labels.cpu().numpy()
-        # cm = confusion_matrix(y_labels, y_pred)
+        # cm = confusion_matrix(y_labels, pred_labels)
         # print("\n Confusion Matrix: \n", cm)
-
-        # Log the confusion matrix as an image summary.
         # figure = plot_confusion_matrix(cm, class_names=['Car', 'Pedestrian', 'Cyclist', 'BG'])
         # classwise_metrics['matrix'] = figure
 
         for cind, cls in enumerate(self.dataset.class_names):
-            y_labels = (assigned_labels == cind).cpu().numpy()
-            y_scores = scores[:, cind].cpu().numpy()
-
-            sem_clf_pr_curve_sem_score_data = {'labels': y_labels, 'predictions': y_scores}
-            classwise_metrics['sem_clf_pr_curve_sem_score'][cls] = sem_clf_pr_curve_sem_score_data
-            classwise_metrics['avg_precision_sem_score'][cls] = average_precision_score(y_labels, y_scores)
-
+            cls_label_mask = y_labels == cind
             cls_pred_mask = pred_labels == cind
-            cls_roi_scores = scores[cls_pred_mask, cind]
+            # cls_pred_mask = argmax_scores == cind
+            # tp_mask = (cls_pred_mask & cls_label_mask)
+
+            sem_clf_pr_curve_data = {'labels': cls_label_mask, 'predictions': scores[:, cind].cpu().numpy()}
+            classwise_metrics['sem_clf_pr_curve'][cls] = sem_clf_pr_curve_data
+
+            cls_roi_scores = max_scores[cls_pred_mask]
             cls_roi_iou_wrt_gt = iou_wrt_gt[cls_pred_mask]
-            # cls_roi_weights = weights[cls_pred_mask]
 
             # Using kitti test class-wise fg thresholds.
             fg_thresh = self.min_overlaps[cind]
@@ -283,9 +283,9 @@ class PredQualityMetrics(Metric):
             classwise_metrics['avg_num_rois_per_sample'][cls] = cls_pred_mask.sum() / self.num_samples
             classwise_metrics['avg_num_gts_per_sample'][cls] = self.num_gts[cind] / self.num_samples
 
-            classwise_metrics['rois_fg_ratio'][cls] = cls_fg_mask.sum() / torch.clip(cls_pred_mask.sum(), min=1)
-            classwise_metrics['rois_uc_ratio'][cls] = cls_uc_mask.sum() / torch.clip(cls_pred_mask.sum(), min=1)
-            classwise_metrics['rois_bg_ratio'][cls] = cls_bg_mask.sum() / torch.clip(cls_pred_mask.sum(), min=1)
+            # classwise_metrics['rois_fg_ratio'][cls] = cls_fg_mask.sum() / max(cls_pred_mask.sum(), 1)
+            # classwise_metrics['rois_uc_ratio'][cls] = cls_uc_mask.sum() / max(cls_pred_mask.sum(), 1)
+            # classwise_metrics['rois_bg_ratio'][cls] = cls_bg_mask.sum() / max(cls_pred_mask.sum(), 1)
 
             add_avg_metric('rois_avg_score', cls_roi_scores)
             add_avg_metric('rois_avg_iou_wrt_gt', cls_roi_iou_wrt_gt)
@@ -296,9 +296,9 @@ class PredQualityMetrics(Metric):
                 classwise_metrics[f'recall_{thresh}'][cls] = self.num_gts_matched[t, cind] / torch.clip(self.num_gts[cind], min=1)
 
             # y_sim_scores = sim_scores[:, cind].cpu().numpy()
-            # sem_clf_pr_curve_sim_score_data = {'labels': y_labels, 'predictions': y_sim_scores}
+            # sem_clf_pr_curve_sim_score_data = {'labels': cls_label_mask, 'predictions': y_sim_scores}
             # classwise_metrics['sem_clf_pr_curve_sim_score'][cls] = sem_clf_pr_curve_sim_score_data
-            # classwise_metrics['avg_precision_sim_score'][cls] = average_precision_score(y_labels, y_sim_scores)
+            # classwise_metrics['avg_precision_sim_score'][cls] = average_precision_score(cls_label_mask, y_sim_scores)
             # cls_sim_mask = sim_labels == cind
             # cls_roi_sim_scores = sim_scores[cls_pred_mask, cind]
             # cls_roi_sim_scores_entropy = Categorical(sim_scores[cls_pred_mask] + torch.finfo(torch.float32).eps).entropy()

pcdet/utils/thresh_algs/adamatch.py
@@ -56,7 +56,7 @@ class AdaMatch(Metric):
         self.temperature = configs.get('TEMPERATURE', 1)  # TODO: Both temperatures should be tuned
         self.temperature_sa = configs.get('TEMPERATURE_SA', 1)
         self.ulb_ratio = configs.get('ULB_RATIO', 0.5)
-
+        self.joint_dist_align = configs.get('JOINT_DIST_ALIGN', False)
         self.enable_ulb_cls_dist_loss = configs.get('ENABLE_ULB_CLS_DIST_LOSS', False)
         self.states_name = ['sem_scores_wa', 'conf_scores_wa', 'gt_labels_wa']
         self.class_names = ['Car', 'Pedestrian', 'Cyclist']
@@ -68,7 +68,7 @@ class AdaMatch(Metric):
 
         # mean_p_model aka p_target (_lbl(mean_p_model)) and p_model (_ulb(mean_p_model))
         self.mean_p_model = {s_name: None for s_name in self.states_name}
-        self.fg_mean_p_model_sa = None
+        self.mean_p_conf_model = {s_name: None for s_name in self.states_name}
         self.mean_p_max_model = {s_name: None for s_name in self.states_name}
         self.labels_hist = {s_name: None for s_name in self.states_name}
         self.ratio = {'AdaMatch': None}
@@ -102,26 +102,27 @@ class AdaMatch(Metric):
 
         return accumulated_metrics
 
-    def _get_mean_p_max_model_and_label_hist(self, max_scores, labels, fg_mask, hist_minlength=3, split=None, type='micro'):
+    def _get_mean_p_max_model_and_label_hist(self, max_scores, labels, mask, hist_minlength=3, split=None, type='micro'):
         if split is None:
             split = ['lbl', 'ulb']
         if isinstance(split, str) and split in ['lbl', 'ulb']:
             _split = _lbl if split == 'lbl' else _ulb
-            fg_max_scores = _split(max_scores, fg_mask)
-            fg_labels = _split(labels, fg_mask)
-            p_max_model = fg_labels.new_zeros(hist_minlength, dtype=fg_max_scores.dtype).scatter_add_(0, fg_labels, fg_max_scores)
-            fg_labels_hist = torch.bincount(fg_labels, minlength=hist_minlength)
-            mean_p_max_classwise = p_max_model / (fg_labels_hist + 1e-6)
+            max_scores = _split(max_scores, mask)
+            labels = _split(labels, mask)
+            labels_hist = torch.bincount(labels, minlength=hist_minlength)
+            p_max_model = labels.new_zeros(hist_minlength, dtype=max_scores.dtype).scatter_add_(0, labels, max_scores)
+            mean_p_max_classwise = p_max_model / (labels_hist + 1e-6)
+
             if type == 'micro':
-                p_max_model = p_max_model.sum() / fg_labels_hist.sum()
+                p_max_model = p_max_model.sum() / labels_hist.sum()
             elif type == 'macro':
                 p_max_model = mean_p_max_classwise.mean()
 
-            fg_labels_hist = fg_labels_hist / fg_labels_hist.sum()
-            return p_max_model, fg_labels_hist, mean_p_max_classwise
+            labels_hist = labels_hist / labels_hist.sum()
+            return p_max_model, labels_hist, mean_p_max_classwise
         elif isinstance(split, list) and len(split) == 2:
-            p_s0, h_s0, pc_s0 = self._get_mean_p_max_model_and_label_hist(max_scores, labels, fg_mask, hist_minlength=hist_minlength, split=split[0], type=type)
-            p_s1, h_s1, pc_s1 = self._get_mean_p_max_model_and_label_hist(max_scores, labels, fg_mask, hist_minlength=hist_minlength, split=split[1], type=type)
+            p_s0, h_s0, pc_s0 = self._get_mean_p_max_model_and_label_hist(max_scores, labels, mask, hist_minlength=hist_minlength, split=split[0], type=type)
+            p_s1, h_s1, pc_s1 = self._get_mean_p_max_model_and_label_hist(max_scores, labels, mask, hist_minlength=hist_minlength, split=split[1], type=type)
             return torch.vstack([p_s0, p_s1]).squeeze(), torch.vstack([h_s0, h_s1]), torch.vstack([pc_s0, pc_s1])
         else:
             raise ValueError(f"Invalid split type: {split}")
@@ -136,9 +137,11 @@ class AdaMatch(Metric):
         acc_metrics = self._accumulate_metrics()
         for sname in ['sem_scores_wa']:
             sem_scores = acc_metrics[sname]
-
+            conf_scores = acc_metrics['conf_scores_wa']
             padding_mask = torch.logical_not(torch.all(sem_scores == 0, dim=-1))
             sem_scores = torch.softmax(sem_scores / (self.temperature_sa if '_sa' in sname else self.temperature), dim=-1)
+            if self.joint_dist_align:
+                sem_scores = sem_scores * conf_scores.unsqueeze(-1)
             max_scores, labels = torch.max(sem_scores, dim=-1)
 
             hist_minlength = sem_scores.shape[-1]
@@ -147,9 +150,14 @@ class AdaMatch(Metric):
             mean_p_model_ulb = _ulb(sem_scores, padding_mask).mean(dim=0)
             mean_p_model = torch.vstack([mean_p_model_lbl, mean_p_model_ulb])
 
+            mean_p_conf_model_lbl = _lbl(conf_scores, padding_mask).mean(dim=0)
+            mean_p_conf_model_ulb = _ulb(conf_scores, padding_mask).mean(dim=0)
+            mean_p_conf_model = torch.vstack([mean_p_conf_model_lbl, mean_p_conf_model_ulb])
+
             self._update_ema('mean_p_max_model', mean_p_max_model, sname)
             self._update_ema('labels_hist', labels_hist, sname)
             self._update_ema('mean_p_model', mean_p_model, sname)
+            self._update_ema('mean_p_conf_model', mean_p_conf_model, sname)
 
             results[f'mean_p_max_model_classwise_lbl/{sname}'] = self._arr2dict(_lbl(mean_p_max_model_classwise), ignore_zeros=True)
             results[f'mean_p_max_model_classwise_ulb/{sname}'] = self._arr2dict(_ulb(mean_p_max_model_classwise), ignore_zeros=True)
@@ -183,13 +191,14 @@ class AdaMatch(Metric):
         results[f'mean_p_max_model_ulb/{sname}'] = _ulb(self.mean_p_max_model[sname])
         results[f'labels_hist_lbl/{sname}'] = self._arr2dict(_lbl(self.labels_hist[sname]))
         results[f'labels_hist_ulb/{sname}'] = self._arr2dict(_ulb(self.labels_hist[sname]))
-
-        unbiased_p_model = self.mean_p_model[sname] / self.labels_hist[sname]
-        unbiased_p_model = unbiased_p_model / unbiased_p_model.sum(dim=-1, keepdim=True)
-        results[f'unbiased_p_model_lbl/{sname}'] = self._arr2dict(_lbl(unbiased_p_model))
-        results[f'unbiased_p_model_ulb/{sname}'] = self._arr2dict(_ulb(unbiased_p_model))
+        results[f'mean_p_conf_model_lbl/{sname}'] = _lbl(self.mean_p_conf_model[sname])
+        results[f'mean_p_conf_model_ulb/{sname}'] = _ulb(self.mean_p_conf_model[sname])
         results[f'mean_p_model_lbl/{sname}'] = self._arr2dict(_lbl(self.mean_p_model[sname]))
         results[f'mean_p_model_ulb/{sname}'] = self._arr2dict(_ulb(self.mean_p_model[sname]))
+        # unbiased_p_model = self.mean_p_model[sname] / self.labels_hist[sname]
+        # unbiased_p_model = unbiased_p_model / unbiased_p_model.sum(dim=-1, keepdim=True)
+        # results[f'unbiased_p_model_lbl/{sname}'] = self._arr2dict(_lbl(unbiased_p_model))
+        # results[f'unbiased_p_model_ulb/{sname}'] = self._arr2dict(_ulb(unbiased_p_model))
 
 
     def draw_dist_plots(self, max_scores, labels, fg_mask, tag, meta_info=''):
@@ -232,23 +241,27 @@ class AdaMatch(Metric):
         prob = getattr(self, p_name)
         prob[tag] = probs if prob[tag] is None else self.momentum * prob[tag] + (1 - self.momentum) * probs
 
-    def get_mask(self, logits):
+    def get_mask(self, conf_scores, sem_logits):
         assert self.thresh_method in ['AdaMatch', 'FreeMatch', 'DebiasedPL'],\
             f'{self.thresh_method} not in list [AdaMatch, FreeMatch, SoftMatch, DebiasedPL]'
 
         if self.iteration_count == 0:
-            scores = torch.softmax(logits / self.temperature, dim=-1)
+            scores = torch.softmax(sem_logits / self.temperature, dim=-1)
+            if self.joint_dist_align:
+                scores = scores * conf_scores.unsqueeze(-1)
             max_scores, labels = torch.max(scores, dim=-1)
             return max_scores > torch.ones_like(max_scores) / len(self.class_names), scores
 
         if self.thresh_method == 'AdaMatch':
-            scores = torch.softmax(logits / self.temperature, dim=-1)
+            scores = torch.softmax(sem_logits / self.temperature, dim=-1)
+            if self.joint_dist_align:
+                scores = scores * conf_scores.unsqueeze(-1)
             scores = self.rectify_sem_scores(scores)
             max_scores, labels = torch.max(scores, dim=-1)
             return max_scores > self._get_threshold(tag=self.lab_thresh_tag), scores
 
         elif self.thresh_method == 'FreeMatch':
-            scores = torch.softmax(logits / self.temperature, dim=-1)
+            scores = torch.softmax(sem_logits / self.temperature, dim=-1)
             max_scores, labels = torch.max(scores, dim=-1)
             thresh = self._get_threshold()
             thresh = thresh.repeat(max_scores.size(0), 1).gather(dim=1, index=labels.unsqueeze(-1)).squeeze()
@@ -259,7 +272,7 @@ class AdaMatch(Metric):
             # I guess the rect_logits can be equivalently implemented as:
             # rect_logits = logits - lambda * torch.log(_ulb(self.mean_p_model['sem_scores_wa']))
             # where lambda is a hyperparameter proportional to the temperature.
-            rect_logits = logits / self.temperature - torch.log(_ulb(self.mean_p_model['sem_scores_wa']))
+            rect_logits = sem_logits / self.temperature - torch.log(_ulb(self.mean_p_model['sem_scores_wa']))
             scores = torch.softmax(rect_logits, dim=-1)
             max_scores, labels = torch.max(scores, dim=-1)
             thresh = self._get_threshold()